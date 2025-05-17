3c177614765e21749390e0ff3809d6392500363b
Andjela Shida <andjelasheeda@gmail.com>
2025-05-17 10:44:30 +0200

    GlobalErrorHandler

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
+import { GlobalErrorHandler } from 'common/global-error-handler';
 
 async function bootstrap() {
   try {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalErrorHandler());
   
   const config = new DocumentBuilder()
   .setTitle('AuctionBay API')
@@ -25,7 +27,7 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
  
   const PORT = process.env.PORT ?? 3000;
 
@@ -33,9 +35,12 @@ async function bootstrap() {
 
   Logger.log(`App is listening on: ${await app.getUrl()}`, 'Bootstrap') ;
   } catch (error) {
-    Logger.error('Error during application startup:', error.message, 'Bootstrap') ;
+    Logger.error(
+      'Error during application startup:',
+       error instanceof Error ? error.message : String(error), 
+       'Bootstrap') ;
     process.exit(1);
-  };
+  }
 }
 
 bootstrap();
